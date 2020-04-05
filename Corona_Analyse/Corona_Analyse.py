4d27e2fa8c8d53d9513bf872791dca8dd07f2bfa
Kersten Schaller <kersten.schaller@outlook.com>
2020-04-05 12:28:10 +0200

    added analysis functions

--- Corona_Analyse/Corona_Analyse.py
+++ Corona_Analyse/Corona_Analyse.py
@@ -1,12 +1,16 @@
 import pprint
 
 from base_Operations import *
+from Analysis_functions import *
 
 eu_corona_data_url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
 corona_file_name = 'corona_data.csv'
 
+############################################################
 print("-- Corona Analysis start --")
-
+############################################################
+# Download and parse
+############################################################
 # download from eu server
 download_corona_data_to_file(eu_corona_data_url, corona_file_name)
 
@@ -16,14 +20,33 @@ corona_data_dictlist = dict_list_from_csv_file(corona_file_name)
 # filter for data of current day
 actual_day_data_dictlist = elements_of_actual_day(corona_data_dictlist)
 
-# filter for country
-country_data_dictlist = elements_of_country(actual_day_data_dictlist, Countries.Germany)
-
+############################################################
+# Analyze
+############################################################
 #show all Countries
-print_countrys(corona_data_dictlist)
+#print_countrys(corona_data_dictlist)
 
-#print info for today germany
-pprint.pprint(elements_of_country(actual_day_data_dictlist, Countries.Germany))	
+
+
+# Deaths today worldwide
+deaths_today = cumulate_deaths(actual_day_data_dictlist)
+print("deaths today worldwide: " + str(deaths_today))
+
+
+# Cumulate deaths worldwide
+deaths_worldwide = cumulate_deaths(corona_data_dictlist)
+print("deaths worldwide: " + str(deaths_worldwide))
+
+
+# Cumulate deaths germany
+country_germany_dictlist = elements_of_country(corona_data_dictlist, Countries.Germany)
+deaths_germany = cumulate_deaths(country_germany_dictlist)
+print("deaths germany: " + str(deaths_germany))
+
+# Cumulate deaths germany today
+country_germany_dictlist_today = elements_of_country(actual_day_data_dictlist, Countries.Germany)
+deaths_germany_today = cumulate_deaths(country_germany_dictlist_today)
+print("deaths germany today: " + str(deaths_germany_today))
 
 
 
